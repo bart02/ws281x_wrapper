282ddbea26bc63ecac9d3a6237eadde65dac7643
Artem Batalov <a@batalov.me>
2020-07-09 11:58:10 +0700

    Inheritance

--- strip/strip.py
+++ strip/strip.py
@@ -5,14 +5,14 @@ from os import getuid
 from time import sleep
 
 
-class Strip:
+class Strip(PixelStrip):
     def __init__(self, num, gpio, **kwargs):
         if getuid() != 0:
             raise PermissionError('Only `root` can use Strip. Try `sudo python <name>`.')
         self.num = num
         self.gpio = gpio
-        self.strip = PixelStrip(self.num, self.gpio, **kwargs)
-        self.strip.begin()
+        super().__init__(self.num, self.gpio, **kwargs)
+        self.begin()
 
     def __setitem__(self, key, color):
         if not isinstance(color, Color):
@@ -24,10 +24,10 @@ class Strip:
                         key.step if key.step is not None else 1)
             for i in key:
                 if i in range(0, self.num):
-                    self.strip.setPixelColor(i, color.dec)
+                    self.setPixelColor(i, color.dec)
         elif isinstance(key, int):
             if key in range(0, self.num):
-                self.strip.setPixelColor(key, color.dec)
+                self.setPixelColor(key, color.dec)
         else:
             TypeError("Invalid argument type")
 
@@ -36,9 +36,9 @@ class Strip:
             key = range(key.start if key.start is not None else 0,
                         key.stop if key.stop is not None else self.num,
                         key.step if key.step is not None else 1)
-            return [Color(self.strip.getPixelColor(i)) for i in key]
+            return [Color(self.getPixelColor(i)) for i in key]
         elif isinstance(key, int):
-            return Color(self.strip.getPixelColor(key))
+            return Color(self.getPixelColor(key))
         else:
             raise TypeError("Invalid argument type")
 
@@ -53,9 +53,6 @@ class Strip:
 
     def clean(self):
         self.__setitem__(slice(None), Color(0))
-
-    def show(self):
-        self.strip.show()
 
     def do_effect(self, effect):
         for st in range(effect.stages):
